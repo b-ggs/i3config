fa4cba3466da9c14a2627e4b8da2714ad8006daa
Boggs <hello.boggs@gmail.com>
2015-12-30 19:06:42 +0800

    Add startup scripts.

---
+++ startup/home_hdmi.sh
@@ -0,0 +1,8 @@
+~/dotfiles/xrandr/x450ldv_spectrum_right_hdmi.sh
+feh --bg-fill ~/assets/background.png
+syndaemon -i 1 -d -t -k
+synclient VertScrollDelta=-67
+synclient HorizScrollDelta=-67
+synclient AccelFactor=0.0754717
+# compton -b --config ~/dotfiles/compton.conf
+setxkbmap -option caps:none
---
+++ startup/home_vga.sh
@@ -0,0 +1,8 @@
+~/dotfiles/xrandr/x450ldv_spectrum_right_vga.sh
+~/dotfiles/scripts/background.sh
+syndaemon -i 1 -d -t -k
+synclient VertScrollDelta=-67
+synclient HorizScrollDelta=-67
+synclient AccelFactor=0.0754717
+# compton -b --config ~/dotfiles/compton.conf
+setxkbmap -option caps:none
